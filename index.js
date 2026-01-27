28fc6334ff39b5c22b89786badb230413d27aafc
Koki Ito <kokiito0926@gmail.com>
2026-01-27 20:56:21 +0900

    feat: 🎸 入力補完の機能を少しだけ実装した
    ユーザーが定義したコマンドの箇所だけで、入力補完を効かせることができたらいいのかもしれない。
    現状、ファイルのパスなどの入力補完を効かせることができないので、そういった箇所は調べ直したほうがいい。

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
+// いちおう、入力補完のコマンドを書いておいたほうがいい。
+// -oのオプションにdefaultを指定していても、ファイルのパスの入力補完が効かない。
+// >> $ complete -p
+// >> $ complete -r pseudoalias
+// >> $ complete -C "./index.js --config ./example.js --completion" -o default pseudoalias
+// >> 2026/01/27 20:53.
+
+// 入力補完の単語を表示する機能を実装した。
+// >> $ ./index.js --config ./example.js --completion
+// >> 2026/01/27 20:18.
+
 // ホームディレクトリに設定のファイルを保存できるようにする。
 // そのようにすれば、コマンドをより短くすることができると思う。
 // そのような処理を実装するのであれば、os.homedir関数を用いればいいような気がする。
@@ -9,15 +20,19 @@
 // >> $ node ./index.js --config example.js greetings
 // >> $ node ./index.js --config example.js addition 1 2
 
-import { minimist, path, argv } from "zx";
+import { minimist, fs, path, argv } from "zx";
 import { pathToFileURL } from "node:url";
 
 const commands = argv._;
 
 const args = minimist(process.argv.slice(2));
 const config = args.config;
+const completion = args.completion;
 // console.log(argv);
 // console.log(args);
+// process.exit(0);
+
+// console.log("aaaa\nbbbb\ncccc");
 // process.exit(0);
 
 const targetFile = config;
@@ -39,6 +54,22 @@ try {
 	// console.log(subCommand);
 	// console.log(restArgs);
 	// process.exit(0);
+
+	const prevWord = process.argv[process?.argv?.length - 1];
+	const currentWord = process.argv[process?.argv?.length - 2];
+
+	if (completion) {
+		// fs.writeFileSync("./temp.txt", prevWord, "utf-8");
+		// fs.writeFileSync("./temp.txt", currentWord, "utf-8");
+		// process.exit(0);
+
+		if (prevWord === "pseudoalias") {
+			const availableFunctions = Object.keys(aliasModule).filter((k) => k !== "default");
+			console.log(availableFunctions.join("\n"));
+			process.exit(0);
+		}
+		process.exit(0);
+	}
 
 	if (!subCommand) {
 		const availableFunctions = Object.keys(aliasModule).filter((k) => k !== "default");
